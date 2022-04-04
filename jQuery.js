a8fbe4e7aaebf5d36ae0cb6d91f6983bef2b12bb
fanglu1 <fanglu0805@gmail.com>
2022-04-04 15:48:36 -0400

    jQuery assignment

---
+++ jQuery.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+  let name = $("#name");
+  let email = $("#email");
+  let phone = $("#phone");
+
+  let required = new Array(name, email, phone);
+  $("#submit").on("click", function (event) {
+    for (let i = 0; i < required.length; i++) {
+      if (required[i].val() == "") {
+        $("p").text("Please Fill Out Required Fields");
+        $("p").addClass("warning");
+        required[i].prev().addClass("warning");
+      }
+    }
+    if (!$("label").hasClass("warning")) {
+      $("form").remove();
+      $("h2").text("Thanks for your feedback!");
+    }
+  });
+  for (let i = 0; i < required.length; i++) {
+    required[i].on("input", function () {
+      if (!(required[i].val() == "")) {
+        required[i].prev().removeClass("warning");
+      }
+    });
+  }
+});
